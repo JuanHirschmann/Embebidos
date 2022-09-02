c21a9f0fd193b32b098ee1b5198e0978636a7fd6
JuanHirschmann <juanhirschmann@gmail.com>
2022-09-01 21:05:12 -0300

    Update main.cpp

--- guia_1/guia_1_02/main.cpp
+++ guia_1/guia_1_02/main.cpp
@@ -1,24 +1,30 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include "hal/gpio_api.h"
+#include "platform/mbed_toolchain.h"
+#include "gpio_object.h"
 int main()
 {
-    DigitalIn gasDetector(D2); /* Constructor de DigitalIn con parámetro pin D2*/
-    //bool gasDetector;
+    gpio_t gasDetector;
+    gpio_init_in_ex(&gasDetector, D2,PullDown);
+
+    //DigitalIn gasDetector(D2); /* Constructor de DigitalIn con parámetro pin D2*/
+    gpio_t alarmLed;
+    gpio_init_out(&alarmLed, LED1);
     
-    DigitalOut alarmLed(LED1);/* Constructor de DigitalOut con parámetro pin LED1*/
+    //DigitalOut alarmLed(LED1);/* Constructor de DigitalOut con parámetro pin LED1*/
 
-    gasDetector.mode(PullDown);/* Método de DigitalIn para activar resistencia de pulldown*/
-    alarmLed = OFF; /*Asigna a alarmLED el valor OFF */
-
+    //gasDetector.mode(PullDown);/* Método de DigitalIn para activar resistencia de pulldown*/
+    //alarmLed = OFF; /*Asigna a alarmLED el valor OFF */
+    gpio_write(&alarmLed, OFF);
     while (true) { //Ciclo infinito
-        //scanf(D2,gasDetector); 
     
-        if ( gasDetector == ON ) { //Si se detectó gas
-            alarmLed = ON;         // Encender LED
+        if ( gpio_read(&gasDetector) == ON ) { //Si se detectó gas
+            gpio_write(&alarmLed, ON);         // Encender LED
         }
         
-        if ( gasDetector == OFF ) { //Si no se detectó gas
-            alarmLed = OFF;         // Apagar LED
+        if ( gpio_read(&gasDetector) == OFF ) { //Si no se detectó gas
+            gpio_write(&alarmLed, OFF);         // Apagar LED
         }
     }
 }
